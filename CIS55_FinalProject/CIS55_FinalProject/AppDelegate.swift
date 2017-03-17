59a8215a149db955b7265d6cbee4917d8ca480eb
Ankita Saund <anu.saund@gmail.com>
2017-03-17 00:27:23 -0700

    all working

--- CIS55_FinalProject/CIS55_FinalProject/AppDelegate.swift
+++ CIS55_FinalProject/CIS55_FinalProject/AppDelegate.swift
@@ -17,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UITabBar.appearance().tintColor = UIColor.cyan
+        UITabBar.appearance().backgroundColor = UIColor.darkGray
+        UINavigationBar.appearance().barTintColor = UIColor(red: 	0.0/255.0, green: 206.0/255.0, blue: 209.0/255.0, alpha: 1.0)
+        UINavigationBar.appearance().tintColor = UIColor.white
         return true
     }
 
